fe08a26b26af8cadb25ff08ba03be0de0eccbdae
sanao1006 <naosak1006@gmail.com>
2023-09-26 20:38:19 +0900

    release

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,37 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    id("org.jetbrains.dokka").version("1.7.10")
+    id("io.github.gradle-nexus.publish-plugin").version("1.1.0")
 }
 true // Needed to make the Suppress annotation work for the plugins block
+
+extra["signing.keyId"] = ""
+extra["signing.password"] = ""
+extra["signing.key"] = ""
+extra["ossrhUsername"] = ""
+extra["ossrhPassword"] = ""
+extra["sonatypeStagingProfileId"] = ""
+
+val secretPropsFile = project.rootProject.file("local.properties")
+if (secretPropsFile.exists()) {
+    val properties = java.util.Properties().apply {
+        load(secretPropsFile.inputStream())
+    }
+    properties.forEach { name, value -> extra[name as String] = value }
+} else {
+    extra["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    extra["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+    extra["sonatypeStagingProfileId"] = System.getenv("SONATYPE_STAGING_PROFILE_ID")
+    extra["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    extra["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    extra["signing.key"] = System.getenv("SIGNING_KEY")
+}
+
+nexusPublishing.repositories.sonatype {
+    stagingProfileId.set(extra["sonatypeStagingProfileId"] as String)
+    username.set(extra["ossrhUsername"] as String)
+    password.set(extra["ossrhPassword"] as String)
+    nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+    snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+}
--- duck/build.gradle.kts
+++ duck/build.gradle.kts
@@ -2,7 +2,14 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
+    id("org.jetbrains.dokka")
+    `maven-publish`
+    signing
 }
+
+val libraryVersion = "0.5.0"
+val libraryName = "compose-duck"
+val libraryDescription = "This is a UI library for Jetpack Compose using random duck api."
 
 android {
     namespace = "com.sanao.compose.duck"
@@ -46,4 +53,79 @@ dependencies {
     implementation(libs.bundles.image)
     implementation(libs.bundles.other)
     implementation(libs.bundles.test)
+}
+
+
+val androidSourcesJar = tasks.register<Jar>("androidSourcesJar") {
+    archiveClassifier.set("sources")
+    from(android.sourceSets["main"].java.srcDirs)
+}
+
+tasks.dokkaJavadoc {
+    outputDirectory.set(File(buildDir, "dokkaJavadoc"))
+}
+
+val javadocJar = tasks.register<Jar>("dokkaJavadocJar") {
+    dependsOn(tasks.dokkaJavadoc)
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+artifacts {
+    archives(androidSourcesJar)
+    archives(javadocJar)
+}
+
+signing {
+    useInMemoryPgpKeys(
+        rootProject.extra["signing.keyId"] as String,
+        rootProject.extra["signing.key"] as String,
+        rootProject.extra["signing.password"] as String,
+    )
+    sign(publishing.publications)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "io.github.sanao1006"
+                artifactId = libraryName
+                version = libraryVersion
+
+                if (project.plugins.hasPlugin("com.android.library")) {
+                    from(components["release"])
+                } else {
+                    from(components["java"])
+                }
+                artifact(androidSourcesJar)
+                artifact(javadocJar)
+                pom {
+                    name.set(artifactId)
+                    description.set(libraryDescription)
+                    url.set("https://github.com/sanao1006/compose-duck")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("sanao1006")
+                            name.set("sanao1006")
+                            url.set("https://sanao.dev/")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:github.com/sanao1006/compose-duck.git")
+                        developerConnection.set("scm:git:ssh://github.com/sanao1006/compose-duck.git")
+                        url.set("github.com/sanao1006/compose-duck")
+                    }
+                }
+            }
+        }
+    }
 }
